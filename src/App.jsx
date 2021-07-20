06c2dc1b7815b29339b85eae078cffa9326d6eae
Pablo Marino <pmarino2013@gmail.com>
2021-07-19 21:41:05 -0300

    Tabla de Coins, numeral.js y ternarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CoinTable from "./components/CoinTable";
 
 const App = () => {
-  return <div></div>;
+  const [coins, setCoins] = useState({
+    datos: [],
+    loading: true,
+    update: false,
+  });
+
+  useEffect(() => {
+    //acciones
+    getCoins().then((respuesta) => {
+      setCoins({
+        datos: respuesta,
+        loading: false,
+        update: true,
+      });
+
+      // console.log(respuesta);
+    });
+  }, []);
+
+  const getCoins = async () => {
+    const resp = await fetch("https://api.coincap.io/v2/assets?limit=10");
+    const informacion = await resp.json();
+    // console.log(informacion.data);
+    return informacion.data;
+  };
+
+  // getCoins();
+
+  // if (coins.datos.length > 0) {
+  //   console.log("Datos cargados");
+  // } else {
+  //   console.log("loading...");
+  // }
+
+  // coins.datos.length > 0
+  //   ? console.log("Datos cargados")
+  //   : console.log("loading...");
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Coin App</h1>
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col">
+          <CoinTable coins={coins} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
